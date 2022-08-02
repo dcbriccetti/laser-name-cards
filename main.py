68a5c37c00c6648a6ea6d53414e5291f2603ee1f
Dave Briccetti <daveb@davebsoft.com>
2022-08-02 10:13:46 -0700

    Improve code quality

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from math import ceil
 from typing import Iterator
 from flask import Flask, render_template, Response, request
 
-Card = tuple[str, str, str]
+Card = tuple[str, str, str]       # x, y, name
+Line = tuple[str, str, str, str]  # x1, y1, x2, y2
 
 # All units are mm except where explicitly stated
 BED_WIDTH = 609.6  # (24″)
@@ -15,23 +16,28 @@ HALF_TEXT_HEIGHT = 7  # For vertical centering.  Todo Calculate this from the fo
 
 app = Flask('Laser Name Card Generator')
 
-def create_cards(names: list[str]) -> Iterator[Card]:
-    for i, name in enumerate(names):
-        row = i // NUM_CARDS_PER_LINE
-        col = i % NUM_CARDS_PER_LINE
-        yield f'{col * CARD_WIDTH + HALF_CARD_WIDTH:.2f}mm', \
-              f'{row * CARD_HEIGHT + HALF_CARD_HEIGHT + HALF_TEXT_HEIGHT:.2f}mm', name
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/cards', methods=['POST'])
 def cards():
-    names = request.form['names'].split()
-    cards = list(create_cards(names))
-    num_rows = int(ceil(len(cards) / NUM_CARDS_PER_LINE))
-    horz_lines = [
+    names: list[str] = request.form['names'].split()
+    num_rows = int(ceil(len(names) / NUM_CARDS_PER_LINE))
+    svg: str = render_template('cards.svg', cards=list(create_cards(names)), lines=create_lines(num_rows))
+    write_svg_for_debugging(svg)
+    return Response(svg, content_type='image/svg+xml')  # todo get content type header working
+
+def create_cards(names: list[str]) -> Iterator[Card]:
+    for i, name in enumerate(names):
+        row = i // NUM_CARDS_PER_LINE
+        col = i %  NUM_CARDS_PER_LINE
+        x = col * CARD_WIDTH + HALF_CARD_WIDTH
+        y = row * CARD_HEIGHT + HALF_CARD_HEIGHT + HALF_TEXT_HEIGHT
+        yield f'{x:.2f}mm', f'{y:.2f}mm', name
+
+def create_lines(num_rows: int) -> list[Line]:
+    horizontal_lines = [
         (
             '0',
             f'{y * CARD_HEIGHT}mm',
@@ -40,7 +46,7 @@ def cards():
         )
         for y in range(num_rows + 1)
     ]
-    vert_lines = [
+    vertical_lines = [
         (
             f'{x * CARD_WIDTH}mm',
             '0',
@@ -49,9 +55,7 @@ def cards():
         )
         for x in range(NUM_CARDS_PER_LINE + 1)
     ]
-    svg: str = render_template('cards.svg', cards=cards, lines=vert_lines + horz_lines)
-    write_svg_for_debugging(svg)
-    return Response(svg, content_type='image/svg+xml')  # todo get content type header working
+    return vertical_lines + horizontal_lines
 
 def write_svg_for_debugging(svg):
     with open('output.svg', 'w') as f:
