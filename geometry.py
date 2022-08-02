6274fa4ce51007ccd584b270c9701923a8cc2277
Dave Briccetti <daveb@davebsoft.com>
2022-08-02 13:43:34 -0700

    Use a more dynamic structure to hold geometry values

---
+++ geometry.py
@@ -0,0 +1,18 @@
+class Geometry:
+    # All units are mm except where stated otherwise
+    bed_width: float
+    num_cards_per_line: int
+    card_height: int
+    font_size: int
+    half_card_height: int
+    card_width: float
+    half_card_width: float
+
+    def __init__(self, bed_width: float, num_cards_per_line: int, card_height: int, font_size: int) -> None:
+        self.bed_width = bed_width
+        self.num_cards_per_line = num_cards_per_line
+        self.card_height = card_height
+        self.font_size = font_size
+        self.half_card_height = card_height // 2
+        self.card_width = bed_width / num_cards_per_line
+        self.half_card_width = self.card_width / 2
--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from math import ceil
 from typing import Iterator
 from flask import Flask, render_template, Response, request
 
+from geometry import Geometry
+
 Card = tuple[str, str, str]       # x, y, name
 Line = tuple[str, str, str, str]  # x1, y1, x2, y2
-
-# All units are mm except where explicitly stated
-BED_WIDTH = 609.6  # (24″)
-NUM_CARDS_PER_LINE = 5
-CARD_WIDTH = BED_WIDTH / NUM_CARDS_PER_LINE
-HALF_CARD_WIDTH = CARD_WIDTH / 2
-CARD_HEIGHT = 40
-HALF_CARD_HEIGHT = CARD_HEIGHT / 2
 
 app = Flask('Laser Name Card Generator')
 
@@ -21,39 +15,40 @@ def index():
 
 @app.route('/cards', methods=['POST'])
 def cards():
+    geom = Geometry(bed_width=609.6, num_cards_per_line=5, card_height=40, font_size=60)
     names: list[str] = request.form['names'].split()
     font = request.form['font']
-    num_rows = int(ceil(len(names) / NUM_CARDS_PER_LINE))
-    svg: str = render_template('cards.svg', cards=list(create_cards(names)), lines=create_lines(num_rows), font=font)
+    num_rows = int(ceil(len(names) / geom.num_cards_per_line))
+    svg: str = render_template('cards.svg', cards=list(create_cards(names, geom)), lines=create_lines(num_rows, geom), font=font)
     write_svg_for_debugging(svg)
     return Response(svg, content_type='image/svg+xml')
 
-def create_cards(names: list[str]) -> Iterator[Card]:
+def create_cards(names: list[str], geom: Geometry) -> Iterator[Card]:
     for i, name in enumerate(names):
-        row = i // NUM_CARDS_PER_LINE
-        col = i %  NUM_CARDS_PER_LINE
-        x = col * CARD_WIDTH + HALF_CARD_WIDTH
-        y = row * CARD_HEIGHT + HALF_CARD_HEIGHT
+        row = i // geom.num_cards_per_line
+        col = i %  geom.num_cards_per_line
+        x = col * geom.card_width  + geom.half_card_width
+        y = row * geom.card_height + geom.half_card_height
         yield f'{x:.2f}mm', f'{y:.2f}mm', name
 
-def create_lines(num_rows: int) -> list[Line]:
+def create_lines(num_rows: int, geom: Geometry) -> list[Line]:
     horizontal_lines: list[Line] = [
         (
             '0',
-            f'{y * CARD_HEIGHT}mm',
-            f'{BED_WIDTH}mm',
-            f'{y * CARD_HEIGHT}mm'
+            f'{y * geom.card_height}mm',
+            f'{geom.bed_width}mm',
+            f'{y * geom.card_height}mm'
         )
         for y in range(num_rows + 1)
     ]
     vertical_lines: list[Line] = [
         (
-            f'{x * CARD_WIDTH}mm',
+            f'{x * geom.card_width}mm',
             '0',
-            f'{x * CARD_WIDTH}mm',
-            f'{num_rows * CARD_HEIGHT}mm'
+            f'{x * geom.card_width}mm',
+            f'{num_rows * geom.card_height}mm'
         )
-        for x in range(NUM_CARDS_PER_LINE + 1)
+        for x in range(geom.num_cards_per_line + 1)
     ]
     return vertical_lines + horizontal_lines
 
