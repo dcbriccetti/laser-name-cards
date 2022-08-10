10ee04999169897c2798e05ff3b6768f2bbfbb5e
Dave Briccetti <daveb@davebsoft.com>
2022-08-10 13:37:26 -0700

    Add option to show the cutting stroke thicker in the preview

--- app/main.py
+++ app/main.py
@@ -14,10 +14,11 @@ def cards():
     form = request.form
     geometry = Geometry.from_form(form)
     names: list[str] = form['names'].split('\r\n')
+    cut_stroke_width = '1pt' if 'bigger_preview_stroke' in form else form['cut_stroke_width']
     svg: str = render_template('cards.svg',
                                cards=list(Card.create_cards(names, geometry)),
                                font_family=form['font_family'],
                                geometry=geometry,
-                               cut_stroke_width=form['cut_stroke_width'],
+                               cut_stroke_width=cut_stroke_width,
                                cut_stroke_color=form['cut_stroke_color'])
     return Response(svg, content_type='image/svg+xml')
